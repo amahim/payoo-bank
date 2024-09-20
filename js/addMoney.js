c758c7c12e4a93ebf23648a0dfd72930414495f0
amahim <arifbin71@gmail.com>
2024-09-20 15:48:34 +0600

    v2

--- js/addMoney.js
+++ js/addMoney.js
@@ -1,3 +1,4 @@
+
 document.getElementById('add-money-btn').addEventListener('click',function(event){
     event.preventDefault();
     const inputAddMoney = document.getElementById('input-add-money').value;
@@ -11,4 +12,26 @@ document.getElementById('add-money-btn').addEventListener('click',function(event
     }else{
         alert('Input pin correctly')
     }
+
+    const inputTable = document.getElementById('input-table')
+    const tr = document.createElement('tr')
+    inputTable.appendChild(tr)
+    tr.className = "bg-green-500 text-white"
+
+    // th1 serial
+    const th1 = document.createElement('th')
+    tr.appendChild(th1)
+    serialSum++;
+    th1.innerText = serialSum;
+
+    // th2 action
+    const th2 = document.createElement('th')
+    tr.appendChild(th2)
+    th2.innerText = 'Cash In';
+
+    // th3 amount
+    const th3 = document.createElement('th')
+    tr.appendChild(th3)
+    th3.innerText = inputMoneyNumber;
+
  })
--- js/cashout.js
+++ js/cashout.js
@@ -7,9 +7,29 @@ document.getElementById('cashout-btn').addEventListener('click',function(event){
         const currentBalance = document.getElementById('current-balance').innerText;
         const currentBalanceNumber = parseFloat(currentBalance)
         const newTotalBalance = currentBalanceNumber - inputCashOutNumber;
-        console.log(newTotalBalance)
         document.getElementById('current-balance').innerText = newTotalBalance
     }else{
         alert('Input pin correctly')
     }
+
+    const inputTable = document.getElementById('input-table')
+    const tr = document.createElement('tr')
+    inputTable.appendChild(tr)
+    tr.className = "bg-red-500 text-white"
+
+    // th1 serial
+    const th1 = document.createElement('th')
+    tr.appendChild(th1)
+    serialSum++;
+    th1.innerText = serialSum;
+
+    // th2 action
+    const th2 = document.createElement('th')
+    tr.appendChild(th2)
+    th2.innerText = 'Cash Out';
+
+    // th3 amount
+    const th3 = document.createElement('th')
+    tr.appendChild(th3)
+    th3.innerText = inputCashOutNumber;
  })
